7ff864d9f3bef4bd3915980b818e4b812b134f80
kaito <103306412+kaitokiddo02@users.noreply.github.com>
2023-06-25 18:51:41 +0800

    Delete staffID_seq.sql

--- DATABASE/staffID_seq.sql
+++
@@ -1,8 +0,0 @@
---------------------------------------------------------
---  File created - Sunday-June-25-2023   
---------------------------------------------------------
---------------------------------------------------------
---  DDL for Sequence STAFFID_SEQ
---------------------------------------------------------
-
-   CREATE SEQUENCE  "HOMESTAY"."STAFFID_SEQ"  MINVALUE 1 MAXVALUE 50000 INCREMENT BY 1 START WITH 8 NOCACHE  NOORDER  NOCYCLE ;
