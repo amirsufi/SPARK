1cfc1d7410c384806eaf8ae3448693234cb53d0d
kaito <103306412+kaitokiddo02@users.noreply.github.com>
2023-06-25 18:51:20 +0800

    Delete homestayID_seq.sql

--- DATABASE/homestayID_seq.sql
+++
@@ -1,8 +0,0 @@
---------------------------------------------------------
---  File created - Sunday-June-25-2023   
---------------------------------------------------------
---------------------------------------------------------
---  DDL for Sequence HOMESTAYID_SEQ
---------------------------------------------------------
-
-   CREATE SEQUENCE  "HOMESTAY"."HOMESTAYID_SEQ"  MINVALUE 1 MAXVALUE 50000 INCREMENT BY 1 START WITH 1 NOCACHE  NOORDER  NOCYCLE ;
