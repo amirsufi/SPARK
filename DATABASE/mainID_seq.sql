f33cec79cbe427dbfc6b850a8f09ff0afdc0df42
kaito <103306412+kaitokiddo02@users.noreply.github.com>
2023-06-25 18:51:31 +0800

    Delete mainID_seq.sql

--- DATABASE/mainID_seq.sql
+++
@@ -1,8 +0,0 @@
---------------------------------------------------------
---  File created - Sunday-June-25-2023   
---------------------------------------------------------
---------------------------------------------------------
---  DDL for Sequence MAINID_SEQ
---------------------------------------------------------
-
-   CREATE SEQUENCE  "HOMESTAY"."MAINID_SEQ"  MINVALUE 1 MAXVALUE 50000 INCREMENT BY 1 START WITH 1 NOCACHE  NOORDER  NOCYCLE ;
