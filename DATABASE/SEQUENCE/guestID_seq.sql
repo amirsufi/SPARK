ff3bf0f8a9ba10d825241b75caab9b639a2f076c
kaito <103306412+kaitokiddo02@users.noreply.github.com>
2023-06-25 18:50:32 +0800

    Add files via upload

---
+++ DATABASE/SEQUENCE/guestID_seq.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  File created - Sunday-June-25-2023   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence GUESTID_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "HOMESTAY"."GUESTID_SEQ"  MINVALUE 1 MAXVALUE 50000 INCREMENT BY 1 START WITH 10 NOCACHE  NOORDER  NOCYCLE ;
---
+++ DATABASE/SEQUENCE/techID_seq.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  File created - Sunday-June-25-2023   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence TECHID_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "HOMESTAY"."TECHID_SEQ"  MINVALUE 1 MAXVALUE 50000 INCREMENT BY 1 START WITH 5 NOCACHE  NOORDER  NOCYCLE ;
