9b2d15fecf89dc366d8b6c8a30cb35a130edaf6a
kaito <103306412+kaitokiddo02@users.noreply.github.com>
2023-06-25 18:46:33 +0800

    Add files via upload

---
+++ DATABASE/bookingID_seq.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  File created - Sunday-June-25-2023   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence BOOKINGID_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "HOMESTAY"."BOOKINGID_SEQ"  MINVALUE 1 MAXVALUE 50000 INCREMENT BY 1 START WITH 2 NOCACHE  NOORDER  NOCYCLE ;
---
+++ DATABASE/homestayID_seq.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  File created - Sunday-June-25-2023   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence HOMESTAYID_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "HOMESTAY"."HOMESTAYID_SEQ"  MINVALUE 1 MAXVALUE 50000 INCREMENT BY 1 START WITH 1 NOCACHE  NOORDER  NOCYCLE ;
